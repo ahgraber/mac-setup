ce801ee1e783f533e6510d3c3c3fda927caa1ec3
Alex Graber <ahgraber@ninerealmlabs.com>
2021-11-01 11:30:20 -0400

    debug

--- scripts/git_dir.sh
+++ scripts/git_dir.sh
@@ -8,9 +8,8 @@ git_select=${git_select:-"y"}
 
 if [[ "$git_select" =~ $noexpr ]]; then
   echo "Skipping git folder setup."
-  echo "WARNING: if Desktop and Documents are cloud synced (iCloud, OneDrive), "
+  echo "WARNING: if Desktop and Documents are cloud synced (iCloud, OneDrive, etc.), "
   echo "saving git repositories in ~/Desktop or ~/Documents may risk sync conflicts!"
-  exit 0
 elif [[ "$git_select" =~ $yesexpr ]]; then
   read -p "Please enter directory name for git repositories folder (to be located in $HOME): " git_dir
   git_dir=${git_dir:-"_GitProjects"}
